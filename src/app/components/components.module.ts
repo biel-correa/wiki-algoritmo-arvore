c70f3b87623be7351105478655a71660eace1e1d
Gabriel Corrêa <gabriel@correa.dev.br>
2022-11-15 20:30:03 -0300

    feature: add angular material and add tooltip for made by

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,7 @@ import {NavbarComponent} from "@components/navbar/navbar.component";
 import { SectionComponent } from '@components/section/section.component';
 import { AlgorithmCardComponent } from './algorithm-card/algorithm-card.component';
 import {RouterModule} from "@angular/router";
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 const components = [
   NavbarComponent,
@@ -16,7 +17,8 @@ const components = [
   exports: [...components],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatTooltipModule
   ]
 })
 export class ComponentsModule { }
