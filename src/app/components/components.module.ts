77bab1ea30383f0abf3e4bb428f680e69a29de20
Gabriel Corrêa <gabriel@correa.dev.br>
2022-11-15 19:52:30 -0300

    feature: create section component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavbarComponent} from "@components/navbar/navbar.component";
+import { SectionComponent } from '@components/section/section.component';
 
 const components = [
-  NavbarComponent
+  NavbarComponent,
+  SectionComponent
 ]
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomePageComponent} from "@pages/home-page/home-page.component";
+import {ComponentsModule} from "@components/components.module";
 
 const components = [
   HomePageComponent
 ]
 
 @NgModule({
-  declarations: components,
-  exports: components,
+  declarations: [...components],
+  exports: [...components],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
