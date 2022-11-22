c6ab1e05038378b0b2900e4bb94d741b8aace1d9
biel-correa <biel-correa@users.noreply.github.com>
2022-11-22 20:35:29 +0000

    deploy: 6127b8c6bc5d279d4d5c359d1d423dcdd9483bc5

--- src/app/models/algorithm.model.ts
+++
@@ -1,7 +0,0 @@
-export default class AlgorithmModel {
-  id: number;
-  title: string;
-  description: string;
-  utilization: string;
-  video?: string;
-}
--- src/app/models/routes.model.ts
+++
@@ -1,4 +0,0 @@
-export default class ROUTES {
-  static readonly HOME = 'home';
-  static readonly TREE = 'tree/:id';
-}
