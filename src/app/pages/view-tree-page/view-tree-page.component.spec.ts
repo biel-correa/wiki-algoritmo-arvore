6127b8c6bc5d279d4d5c359d1d423dcdd9483bc5
Gabriel Corrêa <gabriel@correa.dev.br>
2022-11-22 17:33:04 -0300

    fix: tests were returning error

--- src/app/pages/view-tree-page/view-tree-page.component.spec.ts
+++ src/app/pages/view-tree-page/view-tree-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { ViewTreePageComponent } from './view-tree-page.component';
 
@@ -6,11 +8,15 @@ describe('ViewTreePageComponent', () => {
   let component: ViewTreePageComponent;
   let fixture: ComponentFixture<ViewTreePageComponent>;
 
+  const activatedRouteMock = {
+    params: of({ id: 1 }),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewTreePageComponent ]
-    })
-    .compileComponents();
+      declarations: [ViewTreePageComponent],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ViewTreePageComponent);
     component = fixture.componentInstance;
